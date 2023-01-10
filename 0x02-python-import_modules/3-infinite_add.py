12c8eac0a21b5d3f0189525697f3bd000cf8d0cf
Alampo <alamindayo@gmail.com>
2023-01-10 15:56:01 -0800

    infinte add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/us/bin/python3
+if __name == "__main__"
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
